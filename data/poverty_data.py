fc6c69c40daf89f671a5aa4f9ba39906815e8ab6
Shodiq Wahyu <odiqwr.labsim@gmail.com>
2025-06-17 13:49:38 +0700

    Create poverty_data.py

---
+++ data/poverty_data.py
@@ -0,0 +1,9 @@
+# option_1
+# access the dataset via the following gdrive link: https://drive.google.com/file/d/1hmFqpjXysoSvWedKNNzy8FQH20d5h_e-/view?usp=sharing or
+
+# option_2
+# enter the following link of the Indonesian Central Statistics Agency: https://www.bps.go.id/id
+# move the cursor to the product menu and select dynamic table
+# filter by category: (demographic and social statistics, economics)
+# filter by subject per category: (workforce, etc.)
+# select one of the tables then specify the year, category, and row title
